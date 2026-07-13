f7280ca7f634634dedcc4925f1dd283bd7d16a01
darshan795 <darshanrout81@gmail.com>
2026-07-13 23:50:35 +0530

    working on the 6th exercise

--- mastering useState/mastering useState/src/App.jsx
+++ mastering useState/mastering useState/src/App.jsx
@@ -8,12 +8,13 @@ import Exercise2 from './components/exercise2'
 import Exercise3 from './components/exercise3'
 import Exercise4 from './components/exercise4'
 import Exercise5 from './components/exercise5'
+import Exercise6 from './components/exercise6'
 function App() {
   
 
   return <>
   <div className='main font-["inter"]  h-screen  w-full bg-gray-200 flex justify-center items-center'>
-  <Exercise5/>
+  <Exercise6/>
   </div>
   </>
 }
---
+++ mastering useState/mastering useState/src/components/exercise6.jsx
@@ -0,0 +1,25 @@
+import React,{useStat} from "react";
+import { FaSearch } from "react-icons/fa";
+
+function Exercise6(){
+    return <>
+    <div className="search-bar flex flex-col justify-between">
+        <div className="header">
+            <h1>Search Technologies</h1>
+        </div>
+        <div className="search">
+             <input className="border px-3 py-2 w-100" placeholder="Search here!!!"></input>
+        </div>
+
+        <div className="">
+            options
+        </div>
+       
+    </div>
+
+
+    
+    </>
+}
+
+export default Exercise6;
