bf95b8706a2d035760b6b8c8c7a6c45f1b171a0f
darshan795 <darshanrout81@gmail.com>
2026-07-14 13:05:47 +0530

    completed the exercise 6 which is  a  search fiter

--- mastering useState/mastering useState/src/components/exercise6.jsx
+++ mastering useState/mastering useState/src/components/exercise6.jsx
@@ -8,28 +8,40 @@ import { FaSearch } from "react-icons/fa";
 function Exercise6(){
     const [search,setSearch]=useState("");
     const arr=["React","Tailwind Css","Advanced Javascript","Html Css"];
+    function handleSearch(event){
 
+        
+        setSearch(event.target.value);
+    
+        
+    }
+    
+     const result=arr.filter((a)=>{
+       return  a.toLowerCase().includes(search.toLowerCase());
+
+     })
+     console.log(result);
     return <>
     <div className="search-bar flex flex-col gap-16 bg-white px-10 py-10 rounded-[10px]">
         <div className="header">
             <h1 className="   flex justify-center items-center gap-4 text-2xl"> Search Technologies  <FaSearch/> </h1>
         </div>
         <div className="search flex justify-center items-center relative ">
-             <input className="border px-12 py-2 rounded-[50px] w-100" placeholder="Search here!!!"></input>
+             <input onChange={handleSearch} className="border px-12 py-2 rounded-[50px] w-100" placeholder="Search here!!!"></input>
              <FaSearch className="h-10 w-10 border px-2 py-2 rounded-[50%]  absolute left-0"/>
         </div>
 
         <div className="technologies flex flex-col gap-2 ">
-            {arr.map((ele,index)=>{
+            {result.length>0?(result.map((ele,index)=>{
                 return<h1 className="bg-zinc-200 px-4 py-2  font-[500] text-[20px] rounded-[10px]" key={index}>{ele}</h1>
 
-            })}
+            })):(<h1 className="bg-zinc-200 px-4 py-2  font-[500] text-[20px] rounded-[10px]">Results are not found!!</h1>)}
             
 
             
-        </div>
+        </div>  
        
-    </div>
+    </div>          
 
 
     
