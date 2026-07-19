1605fe898d7af409ce12704e4ba4b84a656cd64f
darshan795 <darshanrout81@gmail.com>
2026-07-19 21:42:31 +0530

    working on the logic of the  9th exercise

--- mastering useState/mastering useState/src/components/exercise9.jsx
+++ mastering useState/mastering useState/src/components/exercise9.jsx
@@ -5,11 +5,34 @@ import { CiCirclePlus } from "react-icons/ci";
 function Exercise9(){
     const [visible,setVisible]=useState(false);
     console.log(visible);
+    const [question,setQuestion]=useState([{
+        id:1,
+        question:"what is trading?",
+        ans:"it is a fantastic skill which  can be praticed",
+        status:false
+    },
+   {id:2,
+    question:"what is coding ? ",
+    ans:"an art of using the machines",
+    status:false
+
+   } ,{
+    id:3,
+    question:"what is  dsa ?",
+    ans:"an art of  understanding the logics of  the fucking thing mother fucker!!",
+    status:false
+   }])
+   question.map((q)=>{
+    console.log(q.question)
+    console.log(q.ans)
+
+   })
     function handleClick(){
         setVisible((prev)=>!prev);
 
         
     }
+    
 
     return <>
     <div className="main h-screen w-full bg-zinc-300 flex flex-col  justify-center items-center gap-10">
@@ -19,6 +42,7 @@ function Exercise9(){
             
         </div>
         <div className="displays flex flex-col gap-4">
+
             <div className="question1">
             <div className="questions text-[15px] font-[600] bg-[#ECF0FB] w-120 px-5 py-2 rounded-[50px] flex items-center justify-between">hey this one is the question ?<CiCirclePlus className="text-[25px]" /></div>
             <div className="answers bg-white w-120 px-5 py-3 rounded-[10px] text-[12px] text-zinc-700 hidden">This is the answers Lorem ipsum dolor sit, 
@@ -27,6 +51,21 @@ function Exercise9(){
                 explicabo, veritatis aut nam molestias, quod eum, 
                 tenetur nesciunt eaque unde minus ab cupiditate.</div>
                 </div>
+            
+            {question.map((ques)=>{
+                return (
+                
+                 <div className="question1">
+            <div className={`questions text-[15px] font-[600] bg-[#ECF0FB] w-120 px-5 py-2  flex items-center justify-between  ${visible?"rounded-tl-[10px] rounded-tr-[10px] bg-white":"rounded-[50px] "} `}>{ques.question}<CiCirclePlus onClick={handleClick} className="text-[25px]" /></div>
+            <div className={`answers bg-white w-120 px-5 py-3 rounded-bl-[10px]  rounded-br-[10px] text-[12px] text-zinc-700 transition-all delay-10 ${visible?"opacity-100 max-h-[1000px]":"opacity-0 max-h-[0px]"} `}>{ques.ans}</div>
+                </div>)
+
+
+            })}
+
+
+
+
             <div className="question1">
             <div className={`questions text-[15px] font-[600] bg-[#ECF0FB] w-120 px-5 py-2  flex items-center justify-between  ${visible?"rounded-tl-[10px] rounded-tr-[10px] bg-white":"rounded-[50px] "} `}>hey this one is the question ?<CiCirclePlus onClick={handleClick} className="text-[25px]" /></div>
             <div className={`answers bg-white w-120 px-5 py-3 rounded-bl-[10px]  rounded-br-[10px] text-[12px] text-zinc-700 transition-all delay-10 ${visible?"opacity-100 max-h-[1000px]":"opacity-0 max-h-[0px]"} `}>This is the answers Lorem ipsum dolor sit, 
