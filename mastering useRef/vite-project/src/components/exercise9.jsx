0d3e18f6502a4d255236db7dbb928965c09df115
darshan795 <darshanrout81@gmail.com>
2026-09-15 12:28:53 +0530

    completed the exercise1 of useMemo

--- mastering useRef/vite-project/src/components/exercise9.jsx
+++ mastering useRef/vite-project/src/components/exercise9.jsx
@@ -1,9 +1,23 @@
 import React  from "react";
-import {useState,useRef,useEffect} from "react";
+import {useState,useMemo,useRef,useEffect} from "react";
+
 
 function Exercise9(){
+    const [number,setNumber]=useState(0);
+    const [name,setName]=useState("");
+    
+    const val=useMemo(()=>{
+        return number*number;
+    },[number])
+    
     return  (<>
-    <h2 className="bg-blue-400"> hello Motherfucker</h2>
+
+    <h1> Counter  :  {number}</h1>
+    <button  onClick={()=>{setNumber((prev)=>prev+1)}}>increase</button>
+    <h1>{name}</h1>
+    <h1> memeoized val {val}</h1>
+    <button onClick={()=>{setName("darshn")}}>CHange Name</button>
+    
     </>)
 
 }
